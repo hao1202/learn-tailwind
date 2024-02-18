0f6dce8523caf156f5fb5f57a06bba3b3cdc4738
hao1202 <tranvanhao2122003@gmail.com>
2024-02-18 15:21:23 +0700

    JobList

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { UserProfile } from "./One/UserProfile";
+import { JobList } from "./Two/JobList";
 
 export default function App() {
   return (
     <div className="flex h-screen w-full items-center justify-center bg-[#E6EFFA]">
-      <UserProfile />
+      {/* <UserProfile /> */}
+      <JobList />
     </div>
   );
 }
