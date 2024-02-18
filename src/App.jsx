43dd0287b6fd1a4ddc752d6f5acb94ee331b0ef2
hao1202 <tranvanhao2122003@gmail.com>
2024-02-18 13:38:45 +0700

    userprofile

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { UserProfile } from "./One/UserProfile";
+
 export default function App() {
   return (
     <div className="flex h-screen w-full items-center justify-center bg-[#E6EFFA]">
-      <p>Hello</p>
+      <UserProfile />
     </div>
   );
 }
