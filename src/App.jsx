6e5edaf52c9e114ab79a512d2342f7a57e104002
hao1202 <tranvanhao2122003@gmail.com>
2024-02-18 12:27:42 +0700

    update base

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-[#E6EFFA]">
+      <p>Hello</p>
+    </div>
+  );
 }
